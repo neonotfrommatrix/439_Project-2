35b47a6db835e7aadc331998b3c45224377b779d
Natalie E. Ottolia <33526906+neonotfrommatrix@users.noreply.github.com>
2019-03-31 19:28:31 -0700

    Update turingmachine.js
    Added comment about the rest of the logic, let me know if it makes sense

--- 439_project_2/assets/turingmachine.js
+++ 439_project_2/assets/turingmachine.js
@@ -51,3 +51,127 @@ const tm = { // turing machine
         return tm.tape[tm.head.x][tm.head.y];
     }
 }; 
+
+
+
+
+
+// I worked out the logic for the turing machine reading and writing as well as the states
+/*
+
+// Its mostly pseudo code
+
+
+//Initialize the first row
+moveTape(right)	x col /2
+writeHead(num)
+
+for each row:
+   moveTape(down)
+   moveTape(left) x col
+   for each col:
+	moveTape(up)
+	num = readHead()
+	temp.writeHead(num) //to temp[0]
+	moveTape(right)
+	temp.moveTape(right)
+	num = readHead()
+	temp.writeHead(num) //to temp[1]
+	moveTape(right)
+	temp.moveTape(right)
+	num = readHead()
+	temp.writeHead(num) //to temp[2]
+	moveTape(left)
+	moveTape(down)
+	num = evalTemp()
+	writeHead(num)
+
+
+
+
+
+ s0	s1	s3	s7
+ ---	1--	11-	111
+			s8
+			110
+
+		s4	s9
+		10-	101
+	 		s10
+			100
+
+
+	s2	s5	s11
+	0--	01-	011
+			s12
+			010
+
+		s6	s13
+		00-	001
+			s14
+			000
+
+
+
+evalTemp(){
+state = 0
+temp.moveTape(left)
+temp.moveTape(left)
+temp.moveTape(left)
+
+	if state == 0
+	{
+		if temp.readHead() == 1
+			state = 1
+		else
+			state = 2
+		temp.moveTape(right)
+		if state == 1
+		{
+			if temp.readHead() == 1
+				state = 3
+			else
+				state = 4
+			temp.moveTape(right)
+			if state == 3
+			{
+				if temp.readHead() == 1
+					state = 7
+				else
+					state = 8
+			}
+			else if state == 4
+			{
+				if temp.readHead() == 1
+					state = 9
+				else
+					state = 10
+			}
+		}
+		else if state == 2
+		{
+			if temp.readHead() == 1
+				state = 5
+			else
+				state = 6
+			temp.moveTape(right)
+			if state == 5
+			{
+				if temp.readHead() == 1
+					state = 11
+				else
+					state = 12
+			}
+			else if state == 6
+			{
+				if temp.readHead() == 1
+					state = 13
+				else
+					state = 14
+			}
+		}
+	}
+	
+	return (state = 7 || 10 || 12 || 13) 
+}
+*/
